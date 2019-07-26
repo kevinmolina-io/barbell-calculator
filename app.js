bbf0ad23026effdc441f4c7c040b69954e50f2c2
Kevin Molina <kevmo1987@gmail.com>
2019-07-25 22:48:12 -0700

    1) update plate selection event listener to: a) update plate selection array 2) Began plate calculation function 3) created dummy function getCube() to test DOM manipulation.

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
-// /**********************************************************/
-// /********************** UI Controller *********************/
-// /**********************************************************/
+let plates = [];
+let plateCount = [];
 
-var UIController = (function() {
-  var DOMstrings = {
-    inputTotalWeight: "#total-weight",
-    inputBarbellWeight: "#barbell-weight",
-    calcBtn: ".calc-btn",
-    plate55: "#plate-55",
-    plate45: "#plate-45",
-    plate35: "#plate-35",
-    plate25: "#plate-25",
-    plate10: "#plate-10",
-    plate5: "#plate-5",
-    plateTwoHalf: "#plate-2half"
-  };
+var getButtonValue = function($button) {
+  var label = $button.text();
+  $button.text("");
+  var buttonValue = $button.val();
+  $button.text(label);
+  return buttonValue;
+};
 
-  /********************* APIs **************************/
-  return {
-    getInput: function() {
-      return {
-        totalWeight: document.querySelector(DOMstrings.inputBarbellWeight)
-          .value,
-        barbellWeight: document.querySelector(DOMstrings.inputBarbellWeight)
-          .value
-      };
-    },
-
-    getDOMstrings: function() {
-      return DOMstrings;
-    }
-  };
-})();
+var arrayRemove = function(arr, value) {
+  return arr.filter(function(ele) {
+    return ele != value;
+  });
+};
 
 $("button.plate-btn").click(function() {
   $button = $(this);
+  let plateVal = "";
   if ($button.hasClass("plate-btn-selected")) {
     $button.removeClass("plate-btn-selected");
     $button.addClass("plate-btn");
+    plateVal = parseFloat($button.text());
+    plates = arrayRemove(plates, plateVal);
+    console.log(plates);
   } else {
     $button.addClass("plate-btn-selected");
     $button.removeClass("plate-btn");
+    plateVal = parseFloat($button.text());
+    plates.push(plateVal);
+    console.log(plates);
   }
 });
+
+var constructPlateCountArray = function() {
+  for (var i = 0; i < plates.length(); i++) {
+    plateCount.push(0);
+  }
+};
+
+// console.log(plates);
+
+var calcPlates = function() {
+  plates = plates.sort();
+  plates = plates.reverse();
+  let totalWeight = parseFloat(document.getElementById("total-weight").value);
+  const BARBELL_WEIGHT = parseFloat(
+    document.getElementById("barbell-weight").value
+  );
+};
+
+function getCube() {
+  let number = parseInt(document.getElementById("total-weight").value);
+  alert(number * number * number);
+}
